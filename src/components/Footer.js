0c939bc264a1c559b015ab5fb60d2ac63ec603bf
karan sharma <karansharma10293847@gmail.com>
2023-12-30 02:09:56 +0530

    some fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ import { IoGlobeSharp } from "react-icons/io5";
 const Footer = () => {
   return (
     <div className=' h-[60px] w-full  bg-gradient-to-r from-[#30A8B9] to-[#A6D6E1] p-1'>
-    <div className='flex gap-2 items-center ml-6'>
-        <div className='text-white'><BiSolidPhoneCall/></div>
+    <div className='flex gap-2 items-center ml-6 text-white'>
+        <div><BiSolidPhoneCall/></div>
         <div>+91 801 3662456</div>
     </div>
-    <div className='flex gap-2 items-center ml-6'>
-        <div className='text-white'><IoGlobeSharp/></div>
-        <a href="https://www.elegantprodesigns.com" target='_blank'>www.elegantprodesigns.com</a>
+    <div className='flex gap-2 items-center ml-6 text-white'>
+        <div><IoGlobeSharp/></div>
+        <a href="https://www.elegantprodesigns.com" target='_blank' className='border-b-2'>www.elegantprodesigns.com</a>
     </div>
     </div>
   )
